e5dbd535610626d14f8d511a7221cebffafdda10
Ryan Mark <rymark907@gmail.com>
2021-10-28 09:56:35 -0700

    work in progress

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Web 47 Rocks!</h1>`)
+})
+
+app.get('/port', (req, res) => {
+    res.send(`<div>port is ${PORT}</div>`)
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
+
+
+
+
+function cohortRocks() {
+    // console.log(process.argv);
+    console.log(`web ${process.argv[2]} rocks`);
+}
+
+function cohortRocksEnv() {
+    console.log(`home folder is ${process.env.HOME}`)
+    console.log(`My shell is ${process.env.SHELL}`);
+    console.log(`In my environment FOO is ${process.env.FOO}`)
+}
+
+cohortRocksEnv();
